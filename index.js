44e5460228fcd6227c9f96a11609033bdfc0b316
Mohmad <moxmad_saidov@mail.ru>
2023-11-07 16:06:45 +0300

    learned few things

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-var mainText = document.getElementsByClassName("main-text");
-
 const link2 = document.querySelector('.link_container a:nth-child(2)')//1link
-
-var links = document.querySelectorAll('.link_container .link')//getting all links
+const links = document.querySelectorAll('.link_container .link')//getting all links
+const footer = document.querySelector('.footer')
+const mainText = document.querySelector('.main .main-text')
+const main = document.querySelector('.main')
 
 Array.from(links).forEach((link) => {
-    link.textContent = "hello";
+    link.textContent = "changed link";
 })
 
-console.log(link2);
+console.log(footer);
+
+mainText.textContent = "I changed the main text"
+
+let count = 1
+mainText.classList.add("bigger_text")
+if(count==0){
+    mainText.classList.remove("bigger_text")
+}
+
+let second_maintext = document.createElement("div")
+second_maintext.textContent = "i was created in js"
+main.append(second_maintext);
