260205c87ceacd80f72655b4423a229d26a5f329
Mohmad Saidov <moxmad0711@gmail.com>
2023-11-08 23:47:44 +0300

    created calc but without buttons,'ll finish tmrw

--- calc/index.js
+++ calc/index.js
@@ -5,19 +5,7 @@ let calc_block = document.createElement('div');
 calc_block.classList.add('calc_block')
 main.append(calc_block)
 
-let counter = document.createElement('div');
-counter.classList.add('counter')
-counter.textContent = 0;
-calc_block.append(counter)
-
-counter.addEventListener('click', () => {
-    counter.textContent = Number(counter.textContent) + 1;
-    if(Number(counter.textContent) % 10 == 0){
-        counter.classList.add('bigger_counter')
-    }else if(Number(counter.textContent) == 101){
-        counter.classList.add('eliminator')
-    }
-    else{
-        counter.classList.remove('bigger_counter')
-    }
-})
+let calc_display = document.createElement('div');
+calc_display.classList.add('calc_display')
+calc_display.textContent = 0;
+calc_block.append(calc_display)
